d753ab1b5edf99c39c95ace3bea102e2738c281e
oic-shuta <C3004@oic.jp>
2024-11-08 17:46:17 +0900

    敵の処理更新

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,45 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    CircleCollider2D circleCollider;
     Rigidbody2D rbody2D;             // Rigidbody2Dを定義
     float speed = 3f;                // 移動速度を格納する変数
     Transform playerTr;
     
+    Collider2D col;
 
     // Start is called before the first frame update
     void Start()
     {
         rbody2D = GetComponent<Rigidbody2D>();
         playerTr = GameObject.FindGameObjectWithTag("Player").transform;
+        col = GetComponent<CapsuleCollider2D>();
+        circleCollider = col.GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 scale = transform.localScale;
-
-        //プレイヤーとの距離が0.1f未満になったらそれ以上実行しない
-        if (Vector2.Distance(transform.position, playerTr.position) < 0.1f)
+        
+        
+        if(circleCollider.bounds.Contains(playerTr.position))
         {
-            return;
+            // 追ってくる処理をここで書く
+            Vector2 scale = transform.localScale;
+
+            //プレイヤーとの距離が0.1f未満になったらそれ以上実行しない
+            if (Vector2.Distance(transform.position, playerTr.position) < 0.1f)
+            {
+                return;
+            }
+
+            if (playerTr.position.x > scale.x)
+            {
+                scale.x = 1; // 右向き
+                speed = 3;
+            }
+            else if (playerTr.position.x < scale.x)
+            {
+                scale.x = -1; // 左向き
+                speed = -3;
+            }
+
+
+            transform.localScale = scale;
+            rbody2D.velocity = new Vector2(speed, rbody2D.velocity.y);
         }
-
-        if (playerTr.position.x >  scale.x)
-        {
-            scale.x = 1; // 右向き
-            speed = 3;
-        }else if(playerTr.position.x < scale.x)
-        {
-            scale.x = -1; // 左向き
-            speed = -3;
-        }
-
-
-        transform.localScale = scale;
-        rbody2D.velocity = new Vector2(speed, rbody2D.velocity.y);
+        
 
     }
 
