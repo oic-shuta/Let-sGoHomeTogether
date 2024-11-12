e5c167a190f771d44651e3c82bf302dce206380c
oic-shuta <C3004@oic.jp>
2024-11-12 15:00:07 +0900

    シーソーの処理追加

---
+++ Assets/Scripts/Gimmick/Seesaw.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seesaw : MonoBehaviour
+{
+    [SerializeField]
+    private Quaternion maxRotate;
+
+    [SerializeField]
+    private Quaternion minRotate;
+
+    [SerializeField]
+    private GameObject seesawObject;
+
+    
+}
