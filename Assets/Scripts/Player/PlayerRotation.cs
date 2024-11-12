f1f05de9905329c8942333c539ec11deeb2688fe
oic-shuta <C3004@oic.jp>
2024-11-12 14:15:48 +0900

    シーソーオブジェクトの修正

--- Assets/Scripts/Player/PlayerRotation.cs
+++ Assets/Scripts/Player/PlayerRotation.cs
@@ -6,6 +6,8 @@ using UnityEngine.UIElements;
 
 public class PlayerRotation : MonoBehaviour
 {
+    [SerializeField]
+    private PlayerController controller;
 
     //傾くオブジェクト
     [SerializeField]
@@ -13,25 +15,45 @@ public class PlayerRotation : MonoBehaviour
 
     [SerializeField]
     private GameObject playerSprite = null;
+
+    [SerializeField]
+    private bool rotateOn = false;
     private void Start()
     {
         playerSprite = this.gameObject;
     }
     private void Update()
     {
-        playerSprite.transform.rotation = rotationObject.transform.rotation;
+        PlayerRotate();
     }
+
+    private void PlayerRotate()
+    {
+        if (!controller.isOnGround)
+        {
+            rotateOn = false;
+        }
+        if (rotateOn)
+        {
+            playerSprite.transform.rotation = rotationObject.transform.rotation;
+        }
+        else if(!rotateOn)
+        {
+            playerSprite.transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.CompareTag("Seesaw"))//シーソーに乗った時その傾きを得る
         {
             rotationObject = collision.gameObject;
+            rotateOn = true;
         }
         else//それ以外だとまっすぐにする
         {
-            rotationObject = collision.gameObject;
+            rotateOn = false;
         }
     }
 }
