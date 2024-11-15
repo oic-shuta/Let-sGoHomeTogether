3b04aaf2a65ecf2d807d8b59af0713912bbcf0da
oic-shuta <C3004@oic.jp>
2024-11-15 16:49:44 +0900

    スクリプト名称変更

--- Assets/Scripts/Game/CheckPoint.cs
+++ Assets/Scripts/Game/CheckPoint.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnReLoad : MonoBehaviour
+public class CheckPoint : MonoBehaviour
 {
     [SerializeField]
     private GameContoller gameController;
