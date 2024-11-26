54bf52239442705804f8e25a6684774cd288ec22
oic-shuta <C3004@oic.jp>
2024-11-26 17:10:50 +0900

    エラー修正

--- Assets/Scripts/Gimmick/WoodObject.cs
+++ Assets/Scripts/Gimmick/WoodObject.cs
@@ -8,11 +8,8 @@ public class WoodObject : MonoBehaviour
     private Rigidbody2D rig2D;
 
     [SerializeField]
-<<<<<<< Updated upstream
-    private GameObject wood;
-=======
     private GameObject coll;
->>>>>>> Stashed changes
+
 
     [SerializeField]
     private GameObject downWood;
@@ -20,24 +17,19 @@ public class WoodObject : MonoBehaviour
     [SerializeField]
     private bool down = false;
 
-<<<<<<< Updated upstream
-=======
+
     [SerializeField]
     private bool change = false;
 
     [SerializeField]
     private GameObject woodPos;
 
->>>>>>> Stashed changes
     private void Start()
     {
         rig2D = GetComponent<Rigidbody2D>();
 
-<<<<<<< Updated upstream
-        wood = this.gameObject;
-=======
+
         rig2D.bodyType = RigidbodyType2D.Kinematic;
->>>>>>> Stashed changes
     }
 
     private void Update()
@@ -49,9 +41,7 @@ public class WoodObject : MonoBehaviour
     {
         if(down)
         {
-<<<<<<< Updated upstream
-            return;
-=======
+
             rig2D.bodyType = RigidbodyType2D.Dynamic;
             down = false;
             this.gameObject.transform.position = woodPos.transform.position;
@@ -64,7 +54,7 @@ public class WoodObject : MonoBehaviour
             downWood.SetActive(true);
 
             this.gameObject.SetActive(false);
->>>>>>> Stashed changes
+
         }
     }
 
