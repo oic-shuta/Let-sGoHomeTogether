7abce915750ec942c500844f34693a41cefa86ab
oic-shuta <C3004@oic.jp>
2024-11-29 15:39:57 +0900

    攻撃の当たり判定の処理の変更

--- Assets/Scripts/Player/AttackPos.cs
+++ Assets/Scripts/Player/AttackPos.cs
@@ -5,29 +5,65 @@ using UnityEngine;
 public class AttackPos : MonoBehaviour
 {
     [SerializeField]
-    private Collider2D coll2D;
-
-    [SerializeField]
     private PlayerAttack attack;
 
     [SerializeField]
-    private Vector3 ofSet = Vector3.zero;
+    private PlayerController controller;
 
     [SerializeField]
-    public bool movePos = false;
+    private GameObject attackColl;
 
+    //移動開始位置
+    [SerializeField] private Transform strat;
 
+    //移動終了位置
+    [SerializeField] private Transform right;
+    [SerializeField] private Transform left;
+
+    //移動する方向
+    [SerializeField] private Transform dest;
+
+    
+    [SerializeField] private bool on = false;
+
+    //移動速度
+    [SerializeField] private float speed;
     private void Start()
     {
-        coll2D = GetComponent<Collider2D>();
+        attackColl = this.gameObject;
 
-        ofSet = coll2D.offset;
+        dest = right;
     }
-    private void Update()
-    {
-        if (attack.attackPlayer)
-        {
 
+    void Update()
+    {
+        //オンの間移動する
+        if (on)
+        {
+            UpdateMove();        
         }
+
+        if (controller.playerDirection)
+        {
+            dest = right;
+        }
+        else if(!controller.playerDirection)
+        {
+            dest = left;
+        }
+
+        var dist = Vector3.Distance(dest.position,this.transform.position);
+
+        if (!attack.attackPlayer)
+        {
+            attackColl.transform.position = strat.position;
+        }
+    }
+
+    //移動処理
+    private void UpdateMove()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position,
+                dest.position, speed * Time.deltaTime);
     }
 }
