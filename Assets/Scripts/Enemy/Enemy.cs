c8d5bcf5c9eedd406bda46abd84355c04db8ccb2
oic-1E11 <131223090+oic-1E11@users.noreply.github.com>
2024-12-24 17:40:32 +0900

    エネミーの当たり判定調整

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : MonoBehaviour
     Rigidbody2D rbody2D;             // Rigidbody2Dを定義
     float speed = 3f;                // 移動速度を格納する変数
     Transform playerTr;
-
+    
     Collider2D col;
     private Animator anim = null;
     private bool isover;
@@ -37,7 +37,7 @@ public class Enemy : MonoBehaviour
 
         //anim.SetBool("run", true);
 
-        if (circleCollider.bounds.Contains(playerTr.position))
+        if(circleCollider.bounds.Contains(playerTr.position))
         {
             //プレイヤーとの距離が0.1f未満になったらそれ以上実行しない
             if (Vector2.Distance(transform.position, playerTr.position) < 0.1f)
@@ -67,7 +67,7 @@ public class Enemy : MonoBehaviour
         if (isover == true)
         {
             StartCoroutine("Timer");
-        }
+        } 
 
     }
 
@@ -79,7 +79,7 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "WeaponAttack")
+        if(collision.gameObject.tag == "WeaponAttack")
         {
             anim.SetBool("down", true);
             isover = true;
@@ -91,4 +91,7 @@ public class Enemy : MonoBehaviour
             Timer();
         }
     }
+
+
+
 }
